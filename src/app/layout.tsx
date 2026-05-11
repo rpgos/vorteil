fe07b4a931d88d15dd748e6b29380d14a2789123
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2026-05-11 22:06:05 +0200

    chore: add zod, next-themes, remove icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,37 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Vorteil - Amateur League",
-  description: "A competittive amateur league for the sport of your choice.",
-};
+  title: 'Vorteil - Amateur League',
+  description: 'A competittive amateur league for the sport of your choice.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
